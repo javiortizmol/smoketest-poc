276362c0cab9181c0bb6242e9558288631f55a65
javicaylent <javier@caylent.com>
2021-08-10 09:07:45 -0500

    Adding the shell tool

--- api-testing-smoketest-poc/README.md
+++ api-testing-smoketest-poc/README.md
@@ -8,11 +8,6 @@ This tool was selected regarding to the comparative dashboard presents on this l
 - .env files - Contains the env variables referenced on the test cases per environment.
 - [test](test) - This folder contains the test cases automated using [pactum](https://pactumjs.github.io/#/) and [mocha](https://mochajs.org/)
 
-# How to install it?
-```
-npm install
-```
-
 # How to run it?
 
 ### For local environment
--- api-testing-smoketest-poc/run-smoke-test.sh
+++ api-testing-smoketest-poc/run-smoke-test.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 environment="$1"
+format="$2"
+build_id="$3"
 
+if [ "$format" == "" ]; then
+    format="html"
+fi
+if [ "$environment" == "" ]; then
+    environment="local"
+fi
+if [ "$build_id" == "" ]; then
+    build_id="0"
+fi
+
+run_test(){
+    smoketest_command="$1"
+    echo "This is the enviroment: $environment"
+    npm run $smoketest_command --environment=$environment --build=$build_id
+    retVal=$?
+    if [ $retVal -ne 0 ]; then
+        echo "ERROR: Smoke test failed"
+    else
+        echo "INFO: Smoke test success"
+    fi
+    exit $retVal
+}
+
+html_report(){
+    run_test smoke-test
+}
+
+xml_report(){
+    run_test smoke-test-xml
+}
+
+# Main Program
 npm install
 
-npm run smoke-test --environment=$environment
-retVal=$?
-if [ $retVal -ne 0 ]; then
-    echo "ERROR: Smoke test failed"
-else
-    echo "INFO: Smoke test success"
+if [ "$format" == "html" ]; then
+    html_report
 fi
-exit $retVal
+
+if [ "$format" == "xml" ]; then
+    xml_report
+fi
+
